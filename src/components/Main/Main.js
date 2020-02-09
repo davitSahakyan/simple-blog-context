03ce3b88e9e3b11aa7a8b88dc499e6930a783100
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-02-09 13:14:02 +0400

    fix : bugs with creating comment

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,32 +3,53 @@ import './Main.css';
 import Comment from '../Comment/Comment'
 
 class Main extends React.Component{
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state={
       comments : [],
+      isShowingInput : false,
     }
   }
 
   addComment = () =>{
-
+    console.log('clicked')
+    this.setState((prevState) => 
+      ({ isShowingInput: !prevState.isShowingInput })
+    )
   }
 
+
   render(){
-    const { comments } = this.state
-    if(comments == false){
+    const { comments , isShowingInput } = this.state
+    console.log('isSHOWInput' , isShowingInput)
+    if(comments == false && isShowingInput === false ){
       console.log('uuuu')
       return (
       <div className='greeting-container'>
         <div className='greeting'>
             <h1> Hello you have succesfully loged in!!</h1>
             <h2> 
-              <span onClick={this.addComment} >Write</span>{' '}
+              <span onClick={this.addComment} type='button' >Write</span>{' '}
               a first comment!</h2>
         </div>
       </div>
       )
     }
+
+    if(isShowingInput === true){
+       return(
+         <div className='input-part'>
+           <div className='input-container'>
+             <input 
+             type='text' 
+             placeholder='write comment'
+              />
+           </div>
+         </div>
+       )
+    }
+
+
     return(
       <div>
         {comments.map( coment =>{
