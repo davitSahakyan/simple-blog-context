ec9b075e0745f84cfd70b0425deb1a089ab77fb0
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-02-09 12:51:09 +0400

    feat : comment component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import './Main.css';
+import Comment from '../Comment/Comment'
 
 class Main extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      comments : [],
+    }
+  }
+
+  addComment = () =>{
+
+  }
 
   render(){
+    const { comments } = this.state
+    if(comments == false){
+      console.log('uuuu')
+      return (
+      <div className='greeting-container'>
+        <div className='greeting'>
+            <h1> Hello you have succesfully loged in!!</h1>
+            <h2> 
+              <span onClick={this.addComment} >Write</span>{' '}
+              a first comment!</h2>
+        </div>
+      </div>
+      )
+    }
     return(
       <div>
-        Hello you have succesfully loged in!!
+        {comments.map( coment =>{
+          return  <Comment  />
+        })} 
       </div>
     )
   }
