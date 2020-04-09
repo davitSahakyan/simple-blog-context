6f7bed571bef390c0a17f1e85da5dc0db450eea9
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-04-09 19:45:05 +0400

    Refactor change Registration and Navigation to functional components

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,51 +3,46 @@ import "./Navigation.css";
 import { withRouter } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-class Navigation extends React.Component {
-    render() {
-        return (
-            <div className="navigation-container">
-                <div className="navigation">
-                    <div className="left-nav">
-                        <div>
-                            <Link to="/simple-blog">
-                                <span className="icon">
-                                    <i className="fas fa-blog" />
-                                    Blog
-                                </span>
-                            </Link>
-                        </div>
-                        <Link to="/simple-blog/create">
-                            <div className="create-post-btn">create post</div>
+const Navigation = (props) => {
+    return (
+        <div className="navigation-container">
+            <div className="navigation">
+                <div className="left-nav">
+                    <div>
+                        <Link to="/simple-blog">
+                            <span className="icon">
+                                <i className="fas fa-blog" />
+                                Blog
+                            </span>
                         </Link>
                     </div>
-
-                    {this.props.isLoggedIn === true ||
-                    this.props.isLoggedIn === "true" ? (
-                        <div
-                            className="right-nav"
-                            onClick={() =>
-                                this.props.history.push(
-                                    "/simple-blog/log-out-modal"
-                                )
-                            }
-                        >
-                            <div type="button">Log out</div>
-                        </div>
-                    ) : (
-                        <div
-                            className="right-nav"
-                            onClick={() =>
-                                this.props.history.push("/simple-blog/verify")
-                            }
-                        >
-                            <div type="button">Log in</div>
-                        </div>
-                    )}
+                    <Link to="/simple-blog/create">
+                        <div className="create-post-btn">create post</div>
+                    </Link>
                 </div>
+
+                {props.isLoggedIn === true || props.isLoggedIn === "true" ? (
+                    <div
+                        className="right-nav"
+                        onClick={() =>
+                            props.history.push("/simple-blog/log-out-modal")
+                        }
+                    >
+                        <div type="button">Log out</div>
+                    </div>
+                ) : (
+                    <div
+                        className="right-nav"
+                        onClick={() =>
+                            props.history.push("/simple-blog/verify")
+                        }
+                    >
+                        <div type="button">Log in</div>
+                    </div>
+                )}
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default withRouter(Navigation);
--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -2,42 +2,38 @@ import React from "react";
 import "./Registration.css";
 import { withRouter } from "react-router-dom";
 
-class Registration extends React.Component {
-    render() {
-        if (this.props.isLoggedIn) {
-            return (
-                <div className="registartion">
-                    <div className="registartion-box">
-                        <span
-                            className="log-in-btn"
-                            onClick={() =>
-                                this.props.history.push("/simple-blog/create")
-                            }
-                        >
-                            Create post{" "}
-                        </span>
-                        and tell us your story!!!
-                    </div>
-                </div>
-            );
-        }
-
+const Registration = (props) => {
+    if (props.isLoggedIn) {
         return (
             <div className="registartion">
                 <div className="registartion-box">
                     <span
                         className="log-in-btn"
                         onClick={() =>
-                            this.props.history.push("/simple-blog/verify")
+                            props.history.push("/simple-blog/create")
                         }
                     >
-                        Log in{" "}
+                        Create post{" "}
                     </span>
                     and tell us your story!!!
                 </div>
             </div>
         );
     }
-}
+
+    return (
+        <div className="registartion">
+            <div className="registartion-box">
+                <span
+                    className="log-in-btn"
+                    onClick={() => props.history.push("/simple-blog/verify")}
+                >
+                    Log in{" "}
+                </span>
+                and tell us your story!!!
+            </div>
+        </div>
+    );
+};
 
 export default withRouter(Registration);
