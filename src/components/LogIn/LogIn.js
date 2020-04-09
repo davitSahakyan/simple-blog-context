156dac05550ef1e36de40490767b07f1fc3537f3
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-04-09 23:36:34 +0400

    Refactor change Login to functional components

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,140 +1,117 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./LogIn.css";
 import { withRouter } from "react-router-dom";
 
-class LogIn extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: "",
-            isOnline: false,
-            errors: [],
-            isLoggedIn: false
-        };
-    }
+function LogIn(props) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [isOnline, setIsOnline] = useState(false);
+    const [errors, setErrors] = useState([]);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    showValidationError = (element, message) => {
-        this.setState(prevState => ({
-            errors: [...prevState.errors, { element, message }]
-        }));
+    const showValidationError = (element, message) => {
+        setErrors([...errors, { element, message }]);
     };
 
-    clearValidationError = element => {
-        this.setState(prevState => {
-            let newArray = [];
-            for (let err of prevState.errors) {
-                if (element !== err.element) {
-                    newArray.push(err);
-                }
+    const clearValidationError = (element) => {
+        let newArray = [];
+        for (let err of errors) {
+            if (element !== err.element) {
+                newArray.push(err);
             }
-            return { errors: newArray };
-        });
+        }
+        setErrors(newArray);
     };
 
-    handleChange = e => {
+    const handleChange = (e) => {
         if (e.target.id === "username") {
-            this.setState({ username: e.target.value });
-            this.clearValidationError("username");
+            setUsername(e.target.value);
+            clearValidationError("username");
         } else if (e.target.id === "password") {
-            this.setState({ password: e.target.value });
-            this.clearValidationError("password");
+            setPassword(e.target.value);
+            clearValidationError("password");
         }
     };
 
-    submitRegistration = () => {
-        const { username, password } = this.state;
+    let registeredUser = props.users.find(
+        (user) => user.username === username && user.password === password
+    );
+
+    useEffect(() => {
+        if (registeredUser) {
+            give(registeredUser);
+        } else if (username && password) {
+            createNewUser();
+        }
+    }, [isLoggedIn, isOnline]);
+
+    const submitRegistration = () => {
         if (username === "") {
-            this.showValidationError("username", "Username can't be empty");
+            showValidationError("username", "Username can't be empty");
         }
         if (password === "") {
-            this.showValidationError("password", "Password can't be empty");
+            showValidationError("password", "Password can't be empty");
         }
-
-        let registeredUser = this.props.users.find(
-            user => user.username === username && user.password === password
-        );
 
         if (registeredUser) {
-            this.setState(
-                {
-                    isLoggedIn: true,
-                    isOnline: true
-                },
-                () => this.give(registeredUser)
-            );
+            setIsLoggedIn(true);
+            setIsOnline(true);
         } else if (username && password) {
-            this.setState({ isLoggedIn: true, isOnline: true }, () =>
-                this.createNewUser()
-            );
+            setIsLoggedIn(true);
+            setIsOnline(true);
         }
     };
 
-    give = registeredUser => {
-        this.props.changeLoggedUserStatusToOnline(registeredUser);
-        this.props.history.push("/simple-blog");
+    const give = (registeredUser) => {
+        props.changeLoggedUserStatusToOnline(registeredUser);
+        props.history.push("/simple-blog");
     };
 
-    createNewUser = () => {
-        this.props.history.push("/simple-blog");
-        this.props.handleUserInfo(
-            this.state.username,
-            this.state.password,
-            this.state.isOnline
-        );
+    const createNewUser = () => {
+        props.history.push("/simple-blog");
+        props.handleUserInfo(username, password, isOnline);
     };
 
-    render() {
-        let usernameErr = null;
-        let passwordErr = null;
+    let usernameErr = null;
+    let passwordErr = null;
 
-        for (let err of this.state.errors) {
-            if (err.element === "username") {
-                usernameErr = err.message;
-            }
-            if (err.element === "password") {
-                passwordErr = err.message;
-            }
+    for (let err of errors) {
+        if (err.element === "username") {
+            usernameErr = err.message;
         }
-
-        return (
-            <div className="login-container">
-                <div className="login-title-container">
-                    <h2 className="login-title">Log in</h2>
-                </div>
-                <div className="login">
-                    <input
-                        type="text"
-                        placeholder="Name *"
-                        id="username"
-                        className="login-input"
-                        onChange={this.handleChange}
-                    />
-                    <sub className="error">
-                        {" "}
-                        {usernameErr ? usernameErr : ""}{" "}
-                    </sub>
-                    <input
-                        type="password"
-                        placeholder="Password *"
-                        id="password"
-                        className="login-input"
-                        onChange={this.handleChange}
-                    />
-                    <sub className="error">
-                        {" "}
-                        {passwordErr ? passwordErr : ""}{" "}
-                    </sub>
-                    <button
-                        className="login-btn"
-                        onClick={this.submitRegistration}
-                    >
-                        log in
-                    </button>
-                </div>
-            </div>
-        );
+        if (err.element === "password") {
+            passwordErr = err.message;
+        }
     }
+
+    return (
+        <div className="login-container">
+            <div className="login-title-container">
+                <h2 className="login-title">Log in</h2>
+            </div>
+            <div className="login">
+                <input
+                    type="text"
+                    placeholder="Name *"
+                    id="username"
+                    className="login-input"
+                    onChange={handleChange}
+                />
+                <sub className="error"> {usernameErr ? usernameErr : ""} </sub>
+                <input
+                    type="password"
+                    placeholder="Password *"
+                    id="password"
+                    className="login-input"
+                    onChange={handleChange}
+                />
+                <sub className="error"> {passwordErr ? passwordErr : ""} </sub>
+                <button className="login-btn" onClick={submitRegistration}>
+                    log in
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default withRouter(LogIn);
